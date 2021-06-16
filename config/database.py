5cf8256f2b49d97f333811b32b6d8b01d2e43e82
StevenMHernandez <me@stevenmhernandez.com>
2021-06-16 19:44:53 -0400

    add default masonite scaffolding for the survero project

---
+++ config/database.py
@@ -0,0 +1,39 @@
+""" Database Settings """
+
+import os
+
+from masonite.environment import LoadEnvironment, env
+from masoniteorm.query import QueryBuilder
+from masoniteorm.connections import ConnectionResolver
+
+"""
+|--------------------------------------------------------------------------
+| Load Environment Variables
+|--------------------------------------------------------------------------
+|
+| Loads in the environment variables when this page is imported.
+|
+"""
+
+LoadEnvironment()
+
+"""
+The connections here don't determine the database but determine the "connection".
+They can be named whatever you want.
+"""
+
+DATABASES = {
+    'default': env('DB_CONNECTION', 'sqlite'),
+    'sqlite': {
+        'driver': 'sqlite',
+        'database': '/usr/src/db/survero',
+        'prefix': ''
+    },
+    'survero': {
+        'driver': 'sqlite',
+        'database': '/usr/src/db/zotero',
+        'prefix': ''
+    }
+}
+
+DB = ConnectionResolver().set_connection_details(DATABASES)
